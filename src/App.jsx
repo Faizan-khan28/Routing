6424d51e64da8ad184b56acc680c9b4a8c23cd49
faizanKhan <faizankhan46849@gmail.com>
2024-11-14 20:27:10 +0530

    routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
-import About from "./components/pages/Contact";
+import Contact from "./components/pages/Contact";
+import NavBar from "./components/layout/NavBar";
 
 function App() {
 
   return (
     <>
      <BrowserRouter>
+       <NavBar/>
        <Routes>
          <Route path="/" element={<Home/>} />
-         <Route path="/" element={<About/>} />
-         <Route path="/" element={<Contact/>} />
+         <Route path="/About" element={<About/>} />
+         <Route path="/Contact" element={<Contact/>} />
        </Routes>
      </BrowserRouter>
     </>
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,9 +1,51 @@
+import { NavLink, Link } from "react-router-dom";
 
+export default function NavBar() {
+  return (
+    <div>
+      <div className="navbar_start">
+        <nav className="navbar navbar-expand-lg  p-4">
+          <div className="container-fluid">
+            <NavLink className="navbar-brand" to="/">
+              My Brand
+            </NavLink>
 
-export default function NavBar () {
-    return (
-        <div>
-            <h1>this is navBar</h1>
-        </div>
-    )
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/">
+                    Home
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/About">
+                    About
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/Contact">
+                    Contact
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+            <Link className="btn btn-outline-primary" to="/user/add">
+              Add User
+            </Link>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
 }
